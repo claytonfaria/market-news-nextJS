9f8048ac3c70ceaa7fea9209f859a6b702216bd1
Clayton Faria <claytonfaria@gmail.com>
2020-10-11 11:17:32 +0800

    improved css arranging, fonts loading and general accessibility

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,10 @@ class MyDocument extends Document {
             as="style"
           />
           <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
+          <link
             rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
             integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
