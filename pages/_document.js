47bc2141fbb0aa767d5d0f74aa69d17e30dfc854
Clayton Faria <claytonfaria@gmail.com>
2020-10-11 12:38:37 +0800

    changes to font loading

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import GoogleFonts from 'next-google-fonts';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-            rel="preload"
-            as="style"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-            rel="stylesheet"
-          />
+          <GoogleFonts href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" />
           <link
             rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
